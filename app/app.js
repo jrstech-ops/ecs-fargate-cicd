e6f984a2a7fa8dda8afcba972aa399fb06215407
root <root@ip-172-31-25-109.ap-southeast-1.compute.internal>
2025-07-11 17:30:18 +0000

    Trigger deploy for dev

---
+++ app/app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello Jomari from ECS Fargate!'));
+app.listen(3000, () => console.log('App running on port 3000'));
+
