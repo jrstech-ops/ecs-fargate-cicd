24f6485c61d047480e7cc0c0f21e266b645df13c
root <root@ip-172-31-25-109.ap-southeast-1.compute.internal>
2025-07-12 05:24:20 +0000

    Setup CI/CD for dev

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello Jomari from ECS Fargate Updated Application 12/07/2025'));
+app.get('/', (req, res) => res.send('Hello Jomari from ECS Fargate Updated Application 12/07/2025 version 1'));
 app.listen(3000, () => console.log('App running on port 3000'));
 
