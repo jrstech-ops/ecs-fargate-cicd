2998070f6e1aa2162f4d04863864cd125a31290a
root <root@ip-172-31-25-109.ap-southeast-1.compute.internal>
2025-09-22 11:00:07 +0000

    Updated code trying again new

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello Jomari!Welcome back from ECS Fargate ECS '));
+app.get('/', (req, res) => res.send('Hello Jomari!Welcome back from ECS Fargate ECS 2025'));
 app.listen(3000, () => console.log('App running on port 3000'));
 
